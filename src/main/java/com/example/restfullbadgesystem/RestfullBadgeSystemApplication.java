76cc134aa17a3bf5cf28adbaaadcfd9f3bc23624
unsmoker <hasangaly@gmail.com>
2022-05-14 13:49:46 -0500

    Initial commit

---
+++ src/main/java/com/example/restfullbadgesystem/RestfullBadgeSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.restfullbadgesystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfullBadgeSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestfullBadgeSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/restfullbadgesystem/RestfullBadgeSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restfullbadgesystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfullBadgeSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
