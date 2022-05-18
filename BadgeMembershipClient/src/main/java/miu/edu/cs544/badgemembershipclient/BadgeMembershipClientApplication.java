eb05be0bb6d829e44ff9262c0497b357b87402f2
Siddhant Pageni <siddhantpageni@gmail.com>
2022-05-18 17:47:57 -0500

    Initial Commit for Client

---
+++ BadgeMembershipClient/src/main/java/miu/edu/cs544/badgemembershipclient/BadgeMembershipClientApplication.java
@@ -0,0 +1,13 @@
+package miu.edu.cs544.badgemembershipclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BadgeMembershipClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BadgeMembershipClientApplication.class, args);
+    }
+
+}
